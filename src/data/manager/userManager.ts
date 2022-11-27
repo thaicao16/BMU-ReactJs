bbe693c5a6ddcfe16784f8660550839515724ff7
thaicaohdv <thaiduc.cao@yum.com>
2022-11-27 10:10:56 +0700

    update data

---
+++ src/data/manager/userManager.ts
@@ -0,0 +1,20 @@
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+import {DataStorageKey} from '../../constants/DataStorageKey';
+import {UserDto} from '../dto/UserDto';
+
+export const clearUserData = async () => {
+  // await AsyncStorage.removeItem(DataStorageKey.USER_DATA_STORAGE);
+};
+
+export const saveUserData = async (user: UserDto) => {
+  // await AsyncStorage.setItem(
+  //   DataStorageKey.USER_DATA_STORAGE,
+  //   JSON.stringify(user),
+  // );
+};
+
+export const getUserData = async (): Promise<UserDto> => {
+//  const value = await AsyncStorage.getItem(DataStorageKey.USER_DATA_STORAGE);
+ const value = 'value';
+  return (value && JSON.parse(value)) || null;
+};
